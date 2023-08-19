474ca82b5de8a50846e34841ff893b4f7fa0c308
Javier Castro <yoprogramo125@gmail.com>
2023-08-19 15:46:46 -0300

    actualizando rutas

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Card from "../components/Card";
 import Carousel from "../components/Carousel";
+import "../styles/styles.css"
 
 export default function Home() {
   return (
